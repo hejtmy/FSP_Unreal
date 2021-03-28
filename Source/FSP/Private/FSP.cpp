71adbec3ed82eb370fb6581d557f850370226a05
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-03-28 15:25:39 +0200

    Initial commit

---
+++ Source/FSP/Private/FSP.cpp
@@ -0,0 +1,20 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "FSP.h"
+
+#define LOCTEXT_NAMESPACE "FFSPModule"
+
+void FFSPModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FFSPModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FFSPModule, FSP)
