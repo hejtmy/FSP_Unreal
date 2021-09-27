39f101c601d3727a3a944a951bcd277484ae543f
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-09-27 21:29:45 +0200

    Adds ingame menu widget definition

--- Source/FSP/Public/UI/FSPInGamePauseMenu.h
+++ Source/FSP/Public/UI/FSPInGamePauseMenu.h
@@ -3,10 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "FSPManager.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
 #include "FSPInGamePauseMenu.generated.h"
 
+class AFSPManager;
 /**
  * 
  */
@@ -14,8 +15,39 @@ UCLASS()
 class FSP_API UFSPInGamePauseMenu : public UUserWidget
 {
 	GENERATED_BODY()
-
+	
 public:
+	virtual void NativeConstruct() override;
+	
 	UPROPERTY(VisibleAnywhere, Category="FSP")
 	AFSPManager* Manager;
+	
+protected:
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UButton* ButtonResume;
+	
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UButton* ButtonTakeScreenshots;
+		
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UButton* ButtonRecordVideo;
+		
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UButton* ButtonQuit;
+	
+	UFUNCTION(BlueprintNativeEvent, Category="FSP|UI")
+	void Resume();
+	virtual void Resume_Implementation();
+	
+	UFUNCTION(BlueprintNativeEvent, Category="FSP|UI")
+	void RecordVideo();
+	virtual void RecordVideo_Implementation();
+	
+	UFUNCTION(BlueprintNativeEvent, Category="FSP|UI")
+	void TakeScreenshots();
+	virtual void TakeScreenshots_Implementation();
+
+	UFUNCTION()
+	void Quit();
+	
 };
