788545b26a0fca0fdd88eb02cd940f810c5922a6
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-09-27 21:30:05 +0200

    Camera view moved to its own folder

--- Source/FSP/Private/CameraViews/FSPCameraView.cpp
+++ Source/FSP/Private/CameraViews/FSPCameraView.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "FSPCameraView.h"
+#include "CameraViews/FSPCameraView.h"
 
 // Sets default values
 AFSPCameraView::AFSPCameraView()
--- Source/FSP/Private/CameraViews/FSPCameraViewManager.cpp
+++ Source/FSP/Private/CameraViews/FSPCameraViewManager.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "FSPCameraViewManager.h"
+#include "CameraViews/FSPCameraViewManager.h"
 
 // Sets default values
 AFSPCameraViewManager::AFSPCameraViewManager()
